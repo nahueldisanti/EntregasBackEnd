574d5ebc557c1a4a39f408551427772c8e126545
nahuel di santi <nahuel.disanti@gmail.com>
2022-10-19 17:18:27 -0300

    Segunda Entrega bis

--- Segunda Entrega/index.js
+++ Segunda Entrega/index.js
@@ -12,12 +12,12 @@ class Contenedor {
     getById = (id) => {
         const items = JSON.parse(fs.readFileSync('./productos.txt', 'utf-8'));
         const foundItem = items.find( i => i.id === id);
-        console.log(foundItem)
+        console.log(`Este es el articulo encontrado: ${foundItem}`)
     }
 
     getAll = () => {
         const items = JSON.parse(fs.readFileSync('./productos.txt', 'utf-8'));
-        console.log(items)
+        console.log(`Estos son todos los articulos encontrados en la lista: ${items}`)
     }
 
     save = (title, price, thumbnail, object) => {
@@ -32,6 +32,7 @@ class Contenedor {
                 data.push(object);
                 const dataJson = JSON.stringify(data);
                 fs.writeFileSync('./productos.txt', dataJson);
+                console.log(`El articulo ${title} ha sigo agregado con exito`)
 
             } else {
                 this.title = title;
@@ -42,6 +43,7 @@ class Contenedor {
                 items.push(object);
                 const itemsJson = JSON.stringify(items);
                 fs.writeFileSync('./productos.txt', itemsJson);
+                console.log(`El articulo ${title} ha sigo agregado con exito`)
             }
         } catch (error) {
             console.log(error)
@@ -49,14 +51,23 @@ class Contenedor {
     }
 
     deleteById = (id) => {
-        const info = JSON.parse(fs.readFileSync('./productos.txt', 'utf-8'));
-        const newInfoFiltered = info.filter((i) => i.id !== id);
-        const newInfoFilteredJson = JSON.stringify(newInfoFiltered);
-        fs.writeFileSync('./productos.txt', newInfoFilteredJson)
+        try{    
+            const info = JSON.parse(fs.readFileSync('./productos.txt', 'utf-8'));
+            const newInfoFiltered = info.filter((i) => i.id !== id);
+            const newInfoFilteredJson = JSON.stringify(newInfoFiltered);
+            fs.writeFileSync('./productos.txt', newInfoFilteredJson);
+            console.log(`El articulo con id ${id} ha sido eliminado con exito!`)
+        } catch(error) {
+            console.log(error)
+        }
     }
 
     deleteAll = () => {
-        fs.writeFileSync('productos.txt','');
+        try{
+            fs.writeFileSync('productos.txt','');
+        }catch(error){
+            console.log(error)
+        }
     }
 }
 
@@ -68,7 +79,7 @@ contenedor.getById(3);
 contenedor.getAll();
 //Agregando algunos productos a productos.txt.
 contenedor.save('Pinzas precision', 6549.25,"Imagen", contenedor);
-contenedor.save('Batidor de mano', 3654.87,"Imagen", conteendor);
+contenedor.save('Batidor de mano', 3654.87,"Imagen", contenedor);
 contenedor.save('Espatula de plastico', 5647.89,"Imagen", contenedor);
 contenedor.save('Cuchillo pan', 78984.64,"Imagen", contenedor);
 //Eliminando producto ID 2
